189fa0c23cdc34010999fded226adeb7b24d8ed5
Nandini Karnakoti <132219274+NandiniKarnakoti@users.noreply.github.com>
2026-05-17 23:01:51 +0530

    Add problem statement and class for subset sum
    Added a problem statement and initial class structure.

--- Not a subset sum.py
+++ Not a subset sum.py
@@ -3,6 +3,10 @@
 # Date: 16 May 2026 
 # Language: Python
 
+""" Problem: Given an array of positive integers, find the smallest
+positive integer that cannot be represented as the sum
+of elements of any subset of the array. """
+
 class Solution:
     def findSmallest(self, arr):
         # code here
